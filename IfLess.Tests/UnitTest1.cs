a2634efb690f9c06ddb89676719deeabb58bca99
Dawid <dwdkls+bestsoftever@gmail.com>
2022-09-08 01:32:17 +0200

    Initial commit

---
+++ IfLess.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace IfLess.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
+
+class TestService
+{
+
+}
---
+++ IfLess/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace IfLess;
+
+public abstract class Result
+{
+
+}
+
+public class Valid : Result
+{
+
+}
+
+public abstract class ErrorBase : Result
+{
+
+}
+
+public class Error : ErrorBase
+{
+    public string Message { get; init; }
+
+    public Error(string message)
+    {
+        Message = message;
+    }
+}
