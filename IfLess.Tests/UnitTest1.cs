84ab754ffb8dfd607050f22b21623588bf445ce0
Dawid <dwdkls+bestsoftever@gmail.com>
2022-09-08 03:09:58 +0200

    Simplest then flow works

--- IfLess.Tests/UnitTest1.cs
+++ IfLess.Tests/UnitTest1.cs
@@ -1,15 +1,53 @@
 namespace IfLess.Tests;
 
-public class UnitTest1
+public class BasicFlowTests
 {
     [Fact]
-    public void Test1()
+    public void WhenValidInput_ReturnsValidResult()
     {
+        var result = TestService.ReverseString("abc");
 
+        result.Should().BeOfType<Valid<string>>();
+    }
+
+    [Fact]
+    public void WhenFailedInput_ReturnsError()
+    {
+        var result = TestService.ReverseString("  ");
+
+        result.Should().BeOfType<Error>();
+    }
+
+    [Fact]
+    public void WhenError_ThenProperlyPassesIt()
+    {
+        var result = TestService.ReverseString("abc")
+            .Then<string>(s => TestService.ToUpperCase(s));
+
+        result.Should().BeOfType<Valid<string>>();
     }
 }
 
 class TestService
 {
+    public static Result ReverseString(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return new Error("Input value can't be empty");
+        }
 
+        var reversed = new string(input.Reverse().ToArray());
+        return new Valid<string>(reversed);
+    }
+
+    public static Result ToUpperCase(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return new Error("Input value can't be empty");
+        }
+
+        return new Valid<string>(input.ToUpperInvariant());
+    }
 }
--- IfLess/Class1.cs
+++ IfLess/Class1.cs
@@ -1,22 +1,44 @@
 ﻿namespace IfLess;
 
+/*
+ * - sync flow
+ * - unit type
+ * - async flow
+ * - collection flow
+ * - combining errors
+ */
+
+public static class ResultExtensions
+{
+    public static Result Then<TValue>(this Result result, Func<TValue, Result> func)
+    {
+        return result is Valid<TValue> valid ? func(valid.Value) : result;
+    }
+}
+
 public abstract class Result
 {
-
+    public abstract bool IsError { get; }
 }
 
-public class Valid : Result
+public class Valid<TValue> : Result
 {
+    public override bool IsError => false;
 
+    public TValue Value;
+
+    public Valid(TValue data)
+    {
+        Value = data;
+    }
+
+    public static implicit operator Valid<TValue>(TValue data) => new Valid<TValue>(data);
 }
 
-public abstract class ErrorBase : Result
+public class Error : Result
 {
+    public override bool IsError => false;
 
-}
-
-public class Error : ErrorBase
-{
     public string Message { get; init; }
 
     public Error(string message)
