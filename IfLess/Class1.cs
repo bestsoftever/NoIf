c3e8fa7771d5ad59c75d7a3e584aef5eb56aa439
Dawid <dwdkls+bestsoftever@gmail.com>
2022-09-30 01:55:23 +0200

    awesome

--- IfLess/Class1.cs
+++ IfLess/Class1.cs
@@ -4,8 +4,8 @@
  *      so we can't use struct
  * errors SHOULD be able to pass through the whole flow, without necessity to re-mapping - it means errors can't be generic?
  * implicit conversions
+ * handle exceptions in Then
  */
-
 
 /*
  * - sync flow for reference types
@@ -16,42 +16,90 @@
  * - combining errors
  */
 
+public abstract class Result<T> //: IWrong3
+{
+    //public abstract T Value { get; }
+
+    //public static implicit operator T(Result3<T> result) => result.Value;
+
+    //public static implicit operator Right3<T>(T data) => new Right3<T>(data);
+
+    //protected Result3(Wrong3 error)
+    //{
+    //    Error = error;
+    //}
+
+    //protected Result3(T data)
+    //{
+    //    Value = data;
+    //}
+
+    //public static Right3<T> Right(T value)
+    //{
+    //    return new Right3<T>(value);
+    //}
+
+    //public static Wrong3 Wrong(string message)
+    //{
+    //    return new Wrong3(message);
+    //}
+
+    public static implicit operator Result<T>(Wrong wrong) => Wrong<T>.From(wrong);
+
+    public static implicit operator Result<T>(T data) => new Right<T>(data);
+}
+
+public class Right<T> : Result<T>
+{
+    public Right(T value)
+    {
+        Value = value;
+    }
+
+    public T Value { get; }
+
+    public static implicit operator Right<T>(T data) => new Right<T>(data);
+}
+
+public class Wrong<T> : Result<T>
+{
+    public string Message { get; init; }
+
+    public Wrong(string message)
+    {
+        Message = message;
+    }
+
+    public static Wrong<T> From<TInner>(Wrong<TInner> innerWrong)
+    {
+        return new Wrong<T>(innerWrong.Message);
+    }
+
+    public static Wrong<T> From(Wrong innerWrong)
+    {
+        return new Wrong<T>(innerWrong.Message);
+    }
+}
+
+public class Wrong
+{
+    public string Message { get; init; }
+
+    public Wrong(string message)
+    {
+        Message = message;
+    }
+}
 
 public static class ResultExtensions
 {
     public static Result<TOutput> Then<TInput, TOutput>(this Result<TInput> result, Func<TInput, Result<TOutput>> func)
     {
-        return result.IsError ? result.Error : func(result.Value);
-    }
-}
-
-public class Result<TValue>
-{
-    public Error? Error { get; }
-    public TValue? Value { get; }
-    public bool IsError => Error != null;
-
-    public Result(Error error)
-    {
-        Error = error;
-    }
-
-    public Result(TValue data)
-    {
-        Value = data;
-    }
-
-    public static implicit operator Result<TValue>(Error error) => new(error);
-
-    public static implicit operator Result<TValue>(TValue data) => new(data);
-}
-
-public class Error
-{
-    public string Message { get; init; }
-
-    public Error(string message)
-    {
-        Message = message;
+        return result switch
+        {
+            Wrong wrong => wrong,
+            Right<TInput> right => func(right.Value),
+            _ => new Wrong<TOutput>("Never happened!"),
+        };
     }
 }
